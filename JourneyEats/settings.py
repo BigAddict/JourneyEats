726da8842c91065a232e273b8e489bcca72cd6c7
BigAddict <davidnjihia536@gmail.com>
2024-03-23 01:19:08 +0300

    modified:   JourneyEats/settings.py

--- JourneyEats/settings.py
+++ JourneyEats/settings.py
@@ -153,8 +153,3 @@ MEDIA_URL = '/media/'
 LOGIN_URL = 'signin'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'davidnjihia536@gmail.com'
-EMAIL_HOST_PASSWORD = 'Lucy_Ruth_Eve3GOO'
